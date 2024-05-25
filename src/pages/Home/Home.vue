a4e89429a38bb4dc2869290f42d9fecb5b1d43bc
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-25 15:24:49 +0700

    Initial commit

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import NavbarComponent from "./components/NavbarComponent.vue";
+</script>
+
+<template>
+  <div>
+    <NavbarComponent />
+    <h1 class="text-3xl bg-fuchsia-300">Home</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home/components/NavbarComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
+<template>
+  <nav class="flex justify-between p-5">
+    <div>
+      <h1>Navbar</h1>
+    </div>
+    <ul class="flex">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <div>
+      <Button
+          label="Login"
+          class="bg-emerald-600"
+      />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
